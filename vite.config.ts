c3b653965f0edf1eade0a60ff281289d0bf4d2b8
abuzar950 <assassinsj327@gmail.com>
2026-05-07 02:23:48 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig } from 'vite
 
 export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
+   base: '/ANIME-WATCH/',const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
     define: {
