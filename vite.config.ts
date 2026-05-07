c04c9c8360df752a2dbc04286cb400bbfae30e61
abuzar950 <assassinsj327@gmail.com>
2026-05-07 03:09:13 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,14 @@
 
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
-  return {
-    base: '/ANIME-WATCH/',
-    plugins: [react()],
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+export default defineConfig({
+  base: '/ANIME-WATCH/',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
-    },
-  };
-});
+  },
+})
